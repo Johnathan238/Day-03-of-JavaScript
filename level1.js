3be57f0ae6d8da035f5a69bfe410e9cc85a4a669
Johnathan238 <60320817+Johnathan238@users.noreply.github.com>
2022-10-04 20:01:22 -0400

    the result of comparison expression

--- level1.js
+++ level1.js
@@ -1,14 +1,50 @@
-const firstName = 'Thomas'
-const lastName = 'Shelby'
-const country = 'UK'
-const city ='Birginham'
-const isMarried = true
-const year = 1965
+// const firstName = 'Thomas'
+// const lastName = 'Shelby'
+// const country = 'UK'
+// const city ='Birginham'
+// const isMarried = true
+// const year = 1965
 
-console.log(typeof firstName, typeof lastName, typeof country, typeof city, typeof isMarried, typeof year)
+// console.log(typeof firstName, typeof lastName, typeof country, typeof city, typeof isMarried, typeof year)
 
-const isIt10 = '10'
-console.log(isIt10 === 10)
+// const isIt10 = '10'
+// console.log(isIt10 === 10)
 
-const isIt9 = 9.8
-console.log(isIt9 === 10)
+// const isIt9 = 9.8
+// console.log(isIt9 === 10)
+
+// let num = 4 > 3
+// console.log(num)
+
+// let num1 = 4 >= 3
+// console.log(num1)
+
+// let num2 = 4 < 3
+// console.log(num2)
+
+// let num3 = 4 <= 3
+// console.log(num3)
+
+// let num4 = 4 == 4
+// console.log(num4)
+
+// let num5 = 4 === 4
+// console.log(num5)
+
+// let num6 = 4 != 4
+// console.log(num6)
+
+// let num7 = 4 !== 4
+// console.log(num7)
+
+// let num8 = 4 != '4'
+// console.log(num8)
+
+// let num9 = 4 == '4'
+// console.log(num9)
+
+// let num10 = 4 === '4'
+// console.log(num10)
+
+
+
