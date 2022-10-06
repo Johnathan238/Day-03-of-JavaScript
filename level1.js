5f5330051483445b2a5dd63c9ae8f79d5607426d
Johnathan238 <60320817+Johnathan238@users.noreply.github.com>
2022-10-06 17:25:45 -0400

    Getting dates using date object

--- level1.js
+++ level1.js
@@ -31,11 +31,11 @@
 // let num5 = 4 === 4
 // console.log(num5)
 
-let num6 = 4 != 4
-console.log(num6)
+// let num6 = 4 != 4
+// console.log(num6)
 
-let num7 = 4 !== 4
-console.log(num7)
+// let num7 = 4 !== 4
+// console.log(num7)
 
 // let num8 = 4 != '4'
 // console.log(num8)
@@ -46,13 +46,46 @@ console.log(num7)
 // let num10 = 4 === '4'
 // console.log(num10)
 
-let findLength1 = 'python'
-let findLength2 = 'jargon'
+// let findLength1 = 'python'
+// let findLength2 = 'jargon'
 
-console.log(findLength1.length, findLength2.length)
+// console.log(findLength1.length, findLength2.length)
 
-let falsyLength = findLength1 === findLength2
-console.log(falsyLength)
+// let falsyLength = findLength1 === findLength2
+// console.log(falsyLength)
 
 
+/////////////Level 1 - 2nd part/////////////
+// const tOrf = 4 > 3 && 10 < 12
+// console.log(tOrf)
 
+// const tOrf1 = 4 > 3 && 10 > 12
+// console.log(tOrf1)
+
+// const tOrf2 = 4 > 3 || 10 < 12
+// console.log(tOrf2)
+
+// const tOrf3 = 4 > 3 || 10 > 12
+// console.log(tOrf3)
+
+// const tOrf4 = !(4 > 3)
+// console.log(tOrf4)
+
+// const tOrf5 = !(false)
+// console.log(tOrf5)
+
+// const tOrf6 = !(4 > 3 && 10 < 12)
+// console.log(tOrf6)
+
+// const tOrf7 = !(4 > 3 && 10 > 12)
+// console.log(tOrf7)
+
+// const tOrf8 = !(4 === '4')
+// console.log(tOrf8)
+
+// const tOrf9 = !(4 > 3 && 10 > 12)
+// console.log(tOrf9)
+
+/////////////Level 1 - 3rd part/////////////
+const getYear = new Date()
+console.log(getYear.getFullYear())
